663802f159745478051fae4d9e5b20d8df51afe5
Harry Kantas <hkantas@twitter.com>
2019-02-11 15:54:36 +0000

    added install script for the default and low_contrast themes

---
+++ low_contrast.js
@@ -0,0 +1,49 @@
+// CUSTOM THEMES CONFIG
+
+// First make sure the wrapper app is loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+   // Then get its webviews
+   let webviews = document.querySelectorAll(".TeamView webview");
+
+   // Fetch our CSS in parallel ahead of time
+   //const cssPath = 'https://cdn.rawgit.com/widget-/slack-black-theme/master/custom.css';
+   const cssPath = 'https://raw.githubusercontent.com/Nockiro/slack-black-theme/3ea2efdfb96ccc91549837ab237d57104181bbf8/custom.css';
+   let cssPromise = fetch(cssPath).then(response => response.text());
+
+   let customCustomCSS = `
+   :root {
+      /* Modify these to change your theme colors: */
+      --primary: #CCC;
+      --text: #999;
+      --background: #222;
+      --background-elevated: #444;
+   }
+   `
+
+   // Insert a style tag into the wrapper view
+   cssPromise.then(css => {
+      let s = document.createElement('style');
+      s.type = 'text/css';
+      s.innerHTML = css + customCustomCSS;
+      document.head.appendChild(s);
+   });
+
+   // Wait for each webview to load
+   webviews.forEach(webview => {
+      webview.addEventListener('ipc-message', message => {
+         if (message.channel == 'didFinishLoading')
+            // Finally add the CSS into the webview
+            cssPromise.then(css => {
+               let script = `
+                     let s = document.createElement('style');
+                     s.type = 'text/css';
+                     s.id = 'slack-custom-css';
+                     s.innerHTML = \`${css + customCustomCSS}\`;
+                     document.head.appendChild(s);
+                     `
+               webview.executeJavaScript(script);
+            })
+      });
+   });
+});
